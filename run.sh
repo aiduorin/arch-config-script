ded6b77d21ffa5c6379b4972f071f06f0b1045ea
bsx_jzb <bsxhomu@outlook.com>
2024-09-16 07:38:48 +0000

    update run.sh.
    Signed-off-by: bsx_jzb <bsxhomu@outlook.com>

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 if [ "$(id -u)" -ne "0" ]; then
-    echo "请以 root 用户运行此脚本。"
+    echo "please run script in root"
     exit 1
 fi
 
 dae_path="./dae-0.8.0rc1-1-x86_64.pkg.tar.zst"
 dae_config_path="./config.dae"
 
-echo "正在安装透明代理dae..."
+echo "install dae..."
 
 for file in "${dae_path}" "${dae_config_path}" "./sub_links"; do
     if [ ! -e "$file" ]; then
-        echo "$file 不存在。"
+        echo "$file not exists。"
         exit 1
     fi
 done
@@ -25,12 +25,12 @@ sub_links=$(sed ':a;N;$!ba; s/\n/\\n/g' ./sub_links)
 
 sed -i '/subscription {/!b; :a; /}/!{n; b a}; /}/i'"${sub_links}"'' /etc/dae/config.dae
 
-echo "dae安装完成"
+echo "dae installed"
 
-if systemctl enable dae_service && systemctl start dae_service; then
-    echo "dae.service 已成功启用并启动。"
+if systemctl enable dae.service && systemctl start dae.service; then
+    echo "dae.service started"
 else
-    echo "无法启用或启动 dae.service"
+    echo "can't run dae.service"
     exit 1
 fi
 
@@ -39,10 +39,10 @@ attempt=0
 dae_success=0
 
 while [ $attempt -lt $max_retries ]; do
-    echo "检测梯子是否正常..."
+    echo "check dae..."
     response=$(curl -s -o /dev/null -w "%{http_code}" --connect-timeout 5 --max-time 10 www.google.com)
     if [ "$response" -eq 200 ]; then
-        echo "梯子正常"
+        echo "dae is normal"
         dae_success=1
         break
     else
@@ -52,29 +52,29 @@ while [ $attempt -lt $max_retries ]; do
 done
 
 if [ $dae_success == 0 ]; then
-    echo "梯子连不上"
+    echo "dae is error"
     exit 1
 fi
 
-echo "安装node & npm..."
+echo "install node & npm..."
 
 pacman -S --noconfirm nodejs npm
 
 if command -v node >/dev/null 2>&1 && command -v npm >/dev/null 2>&1; then
-    echo "node & npm 已安装"
+    echo "node & npm installed"
 else
-    echo "node & npm 安装失败"
+    echo "node & npm install failed"
     exit 1
 fi
 
-echo "安装zx..."
+echo "install zx..."
 
 npm install -g zx
 
 if command -v zx >/dev/null 2>&1; then
-    echo "zx 已安装"
+    echo "zx installed"
 else
-    echo "zx 安装失败"
+    echo "zx install failed"
     exit 1
 fi
 
