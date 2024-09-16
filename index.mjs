c9ed154d3dc485f4f5a8dd51b990a1995d00bbf7
bsxxu <bsxhomu@outlook.com>
2024-09-16 21:50:54 +0800

    fix: bugs

--- index.mjs
+++ index.mjs
@@ -33,7 +33,7 @@ const installAUR = async (url, afterInstall) => {
     const dir = url.split("/").at(-1).replace(".git", "");
     await $`chmod 777 ${dir}` 
     cd(dir);
-    await $`sudo -u bsx bash -c 'yes | makepkg --syncdeps'`;
+    await $`sudo -u bsx bash -c 'makepkg --syncdeps'`;
     const p = (await $`ls *.pkg.tar.zst 2>/dev/null | grep -v 'debug'`).stdout;
     await $`pacman -U --noconfirm ${p.slice(0, p.length - 1)}`;
     afterInstall && (await afterInstall());
@@ -43,23 +43,26 @@ const installAUR = async (url, afterInstall) => {
 //mirror
 await installPKG("reflector", true);
 await $`reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist`;
-console.log("镜像配置完成");
 await $`pacman -Syu --noconfirm`;
+console.log("mirror OK");
 
 //git
-await installPKG("git", true);
+await installPKG(["git", "less"], true);
 const gitConfigResp = await $`cat ./git.conf`;
 const [gitUser, gitEmail] = gitConfigResp.stdout.split("\n");
-await $`git config --global user.name "${gitUser}"`;
-await $`git config --global user.email "${gitEmail}"`;
+await $`sudo -u bsx bash -c 'git config --global user.name "${gitUser}"'`;
+await $`sudo -u bsx bash -c 'git config --global user.email "${gitEmail}"'`;
 
-await $`ssh-keygen -t rsa -C "${gitEmail}"`;
+await $`sudo -u bsx bash -c 'ssh-keygen -t rsa -C "${gitEmail}"'`;
+await $`sudo -u bsx bash -c 'cat ./ssh.conf > ~/.ssh/config'`
+console.log("git OK");
 
 //update dae
 await installAUR(
   "https://aur.archlinux.org/dae.git",
   async () => await $`systemctl restart dae.service`,
 );
+console.log("dae OK");
 
 //font
 await installPKG(["noto-fonts", "noto-fonts-cjk"]);
@@ -74,30 +77,36 @@ await inTmpDir(async () => {
 });
 
 await $`sudo -u bsx bash -c 'mkdir -p ~/.config/fontconfig/ && cat ./fonts.conf > ~/.config/fontconfig/fonts.conf'`;
+console.log("font OK");
 
 //nvm
 await $`sudo -u bsx bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash'`;
+console.log("nvm OK");
+
 
 //starship
-installPKG("starship");
-await $`sudo -u bsx bash -c 'printf '\neval "$(starship init bash)"\n' >> ~/.bashrc'`;
+await installPKG("starship");
+await $`printf '\neval "$(starship init bash)"\n' | sudo -u bsx bash -c 'tee -a ~/.bashrc > /dev/null'`
+console.log("starship OK");
 
 //bluetooth
-installPKG(["bluez", "bluez-utils"]);
+await installPKG(["bluez", "bluez-utils"]);
 await $`modprobe btusb`;
 await $`systemctl enable bluetooth.service && systemctl start bluetooth.service`;
+console.log("bluetooth OK");
 
 //input method
-installPKG([
+await installPKG([
   "fcitx5-im",
   "fcitx5-chinese-addons",
   "fcitx5-qt",
   "fcitx5-gtk",
   "fcitx5-pinyin-zhwiki",
 ]);
-installAUR("https://aur.archlinux.org/fcitx5-pinyin-moegirl.git");
+await installAUR("https://aur.archlinux.org/fcitx5-pinyin-moegirl.git");
+console.log("input method OK");
 
 //common
-installAUR("https://aur.archlinux.org/google-chrome.git");
-installAUR("https://aur.archlinux.org/visual-studio-code-bin.git");
-installAUR("https://aur.archlinux.org/telegram-desktop-bin.git");
+await installAUR("https://aur.archlinux.org/google-chrome.git");
+await installAUR("https://aur.archlinux.org/visual-studio-code-bin.git");
+await installAUR("https://aur.archlinux.org/telegram-desktop-bin.git");
